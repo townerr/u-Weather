6097b6628498e0845dcf094f1511c6b59ed0d94c
Ryan Towner <townerr@yahoo.com>
2022-02-05 21:15:55 -0600

    fixed responsive sizing/positioning for mobile on some elements

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,29 +2,22 @@ import React from 'react';
 import './Header.css';
 import Search from './Search';
 import WeatherCard from './WeatherCard';
-import { Col, Row } from 'react-bootstrap';
 import { FaCloud } from 'react-icons/fa';
 
 const Header = ({data, setData}) => {
     return (
         <div className="header">
             <div className="container">
-                <div className="title"><FaCloud /> u-WEATHER <FaCloud /></div>
-                <div className="title-under">A Simple Weather App</div>
-                <Search data={data} setData={setData}/>
-                <div className="header-cards">
-                    <Row>
-                        <Col>
-                            <WeatherCard zip="10001" keys={0} />
-                        </Col>
-                        <Col>
-                            <WeatherCard zip="60601" keys={0}/>
-                        </Col>
-                        <Col>
-                            <WeatherCard zip="90001" keys={0}/>
-                        </Col>
-                    </Row>
+                <div className='title-container'>
+                    <div className="title"><FaCloud /> u-WEATHER <FaCloud /></div>
+                    <div className="title-under">A Simple Weather App</div>
+                    <Search data={data} setData={setData}/>
                 </div>
+                <div className="header-container">
+                    <div className="header-card"><WeatherCard  zip="10001" keys={0} /></div>
+                    <div className="header-card"><WeatherCard className="header-card" zip="60601" keys={0}/></div>
+                    <div className="header-card"><WeatherCard className="header-card" zip="90001" keys={0}/></div>
+                </div> 
             </div>
         </div>
     )
